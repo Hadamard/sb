bbbc66a3da9140377ce2d35b385f25ed02c831f0
HAL 9000 <132300160+mysteryxteo@users.noreply.github.com>
2023-12-14 22:17:19 +0100

    Update index.js
    Signed-off-by: HAL 9000 <132300160+mysteryxteo@users.noreply.github.com>

--- src/dashboard/src/pages/Overview/index.js
+++ src/dashboard/src/pages/Overview/index.js
@@ -1,12 +1,41 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Bar } from 'react-chartjs-2';
 
 function RedirectButton() {
   return (
     <Link to="https://hadamard.com" target="_blank">
-      <button>Zu Hadamard.com</button>
+      <button>Visit Hadamard.com</button>
     </Link>
   );
 }
+
+
+
+
+
+const Chart1 = () => {
+  const data = {
+    labels: ['Label 1', 'Label 2', 'Label 3'],
+    datasets: [
+      {
+        label: 'Chart 1 Data',
+        data: [12, 19, 3],
+        backgroundColor: 'rgba(75, 192, 192, 0.2)',
+        borderColor: 'rgba(75, 192, 192, 1)',
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  return (
+    <div style={{ width: '50%', float: 'left' }}>
+      <Bar data={data} />
+    </div>
+  );
+};
+
+
+
 
 export default RedirectButton;
