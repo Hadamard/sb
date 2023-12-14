a871f5e54efc5f6306325525412cf6e1d6cc1a45
HAL 9000 <132300160+mysteryxteo@users.noreply.github.com>
2023-12-14 22:31:13 +0100

    Update index.js
    Signed-off-by: HAL 9000 <132300160+mysteryxteo@users.noreply.github.com>

--- src/dashboard/src/pages/Overview/index.js
+++ src/dashboard/src/pages/Overview/index.js
@@ -1,41 +1,60 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Bar } from 'react-chartjs-2';
-
-function RedirectButton() {
-  return (
-    <Link to="https://hadamard.com" target="_blank">
-      <button>Visit Hadamard.com</button>
-    </Link>
-  );
-}
-
-
-
-
+import React, { useEffect } from 'react';
 
 const Chart1 = () => {
-  const data = {
-    labels: ['Label 1', 'Label 2', 'Label 3'],
-    datasets: [
-      {
-        label: 'Chart 1 Data',
-        data: [12, 19, 3],
-        backgroundColor: 'rgba(75, 192, 192, 0.2)',
-        borderColor: 'rgba(75, 192, 192, 1)',
-        borderWidth: 1,
-      },
-    ],
-  };
+  useEffect(() => {
+    const canvas = document.getElementById('chart1');
+    const ctx = canvas.getContext('2d');
+
+    // Zeichne deinen Chart 1 hier mit Canvas und JavaScript
+    // Beispiel:
+    ctx.fillStyle = 'blue';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+  }, []);
 
   return (
     <div style={{ width: '50%', float: 'left' }}>
-      <Bar data={data} />
+      <canvas id="chart1" width="300" height="200"></canvas>
     </div>
   );
 };
 
+const Chart2 = () => {
+  useEffect(() => {
+    const canvas = document.getElementById('chart2');
+    const ctx = canvas.getContext('2d');
 
+    // Zeichne deinen Chart 2 hier mit Canvas und JavaScript
+    // Beispiel:
+    ctx.fillStyle = 'red';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+  }, []);
+
+  return (
+    <div style={{ width: '50%', float: 'left' }}>
+      <canvas id="chart2" width="300" height="200"></canvas>
+    </div>
+  );
+};
+
+const ChartsContainer = () => {
+  return (
+    <div>
+      <Chart1 />
+      <Chart2 />
+    </div>
+  );
+};
+
+const App = () => {
+  return (
+    <div>
+      <h1>Two Charts Side by Side</h1>
+      <ChartsContainer />
+    </div>
+  );
+};
 
 
 export default RedirectButton;
