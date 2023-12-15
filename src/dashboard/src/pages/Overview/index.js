d90d5abd9f735c2ba51e5308b2ab094bd3bb67fc
HAL 9000 <132300160+mysteryxteo@users.noreply.github.com>
2023-12-15 15:09:46 +0100

    Update index.js
    Signed-off-by: HAL 9000 <132300160+mysteryxteo@users.noreply.github.com>

--- src/dashboard/src/pages/Overview/index.js
+++ src/dashboard/src/pages/Overview/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Bar } from 'react-chartjs-2';
-import { CardElement, useStripe, useElements } from '@stripe/react-stripe-js';
+import { Line } from 'react-chartjs-2';
+import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend } from 'chart.js';
 
 function CheckoutForm() {
   const stripe = useStripe();
@@ -22,31 +22,28 @@ function CheckoutForm() {
 
 
 
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
-const BarChart = () => {
+const LineChart = () => {
   const data = {
     labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'],
     datasets: [
       {
         label: 'Verkäufe 2023 (Mio.)',
         data: [12, 19, 3, 5, 2, 3],
-        backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-          'rgba(255, 159, 64, 0.2)'
-        ],
-        borderColor: [
-          'rgba(255, 99, 132, 1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)'
-        ],
-        borderWidth: 1
+        backgroundColor: 'rgba(255, 255, 255, 0.5)', // Weiß mit leichter Transparenz
+        borderColor: 'rgba(211, 211, 211, 1)', // Hellgrau
+        borderWidth: 2,
+        pointBackgroundColor: 'rgba(255, 255, 255, 1)', // Weiße Datenpunkte
+        pointBorderColor: 'rgba(211, 211, 211, 1)', // Hellgraue Umrandung der Datenpunkte
       }
     ]
   };
@@ -56,10 +53,18 @@ const BarChart = () => {
       y: {
         beginAtZero: true
       }
+    },
+    elements: {
+      line: {
+        tension: 0.4 // Gibt der Linie eine leichte Krümmung
+      },
+      point: {
+        radius: 5 // Größe der Datenpunkte
+      }
     }
   };
 
-  return <Bar data={data} options={options} />;
+  return <Line data={data} options={options} />;
 };
 
-export default BarChart;
+export default LineChart;
