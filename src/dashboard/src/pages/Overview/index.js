7e16564d059c02be42aa95db4cb86f1537ca32f1
HAL 9000 <132300160+mysteryxteo@users.noreply.github.com>
2023-12-15 23:21:26 +0100

    Update index.js
    Signed-off-by: HAL 9000 <132300160+mysteryxteo@users.noreply.github.com>

--- src/dashboard/src/pages/Overview/index.js
+++ src/dashboard/src/pages/Overview/index.js
@@ -1,1 +1,35 @@
+import React, { useState } from 'react';
+
+function SubscriptionComponent() {
+    const [email, setEmail] = useState('');
+    const [subscriptions, setSubscriptions] = useState(null);
+    const [loading, setLoading] = useState(false);
+
+    const fetchSubscriptions = async () => {
+        setLoading(true);
+        try {
+            const response = await fetch(`http://localhost:5000/get-subscriptions?email=${email}`);
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            const data = await response.json();
+            setSubscriptions(data);
+        } catch (error) {
+            console.error('Failed to fetch subscriptions:', error);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    return (
+        <div>
+            <input type="email" value={email} onChange={e => setEmail(e.target.value)} />
+            <button onClick={fetchSubscriptions}>Get Subscriptions</button>
+            {loading && <p>Loading...</p>}
+            {subscriptions && <pre>{JSON.stringify(subscriptions, null, 2)}</pre>}
+        </div>
+    );
+}
+
+export default SubscriptionComponent;
 
