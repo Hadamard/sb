d6806bd4890a4a153ee38f68a8c957bdf5affcc7
HAL 9000 <132300160+mysteryxteo@users.noreply.github.com>
2023-12-15 20:12:41 +0100

    Update Footer.js
    Signed-off-by: HAL 9000 <132300160+mysteryxteo@users.noreply.github.com>

--- src/dashboard/src/layouts/Footer.js
+++ src/dashboard/src/layouts/Footer.js
@@ -9,7 +9,7 @@ const FooterView = () => (
     <GlobalFooter
       copyright={
         <Fragment>
-          Copyright <CopyrightOutlined /> Hadamard Corp., All Rights Reserved.
+          Copyright <CopyrightOutlined /> Hadamard Corp.
         </Fragment>
       }
     />
