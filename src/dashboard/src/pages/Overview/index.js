50bcd4276f29a4fa836a07b4e96ac7e095d5d23c
HAL 9000 <132300160+mysteryxteo@users.noreply.github.com>
2023-12-15 20:28:01 +0100

    Update index.js
    Signed-off-by: HAL 9000 <132300160+mysteryxteo@users.noreply.github.com>

--- src/dashboard/src/pages/Overview/index.js
+++ src/dashboard/src/pages/Overview/index.js
@@ -1,63 +1,49 @@
-import React from 'react';
-import { Line } from 'react-chartjs-2';
-import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend } from 'chart.js';
+import React, { useState, useEffect } from 'react';
 
+const UserPlanComponent = () => {
+  const [userPlan, setUserPlan] = useState([]);
 
+  // Hier können Sie Daten aus Ihrer API oder Datenquelle abrufen
+  useEffect(() => {
+    // Beispiieldaten
+    const planData = [
+      { tag: 'Montag', aktivität: 'Laufen' },
+      { tag: 'Dienstag', aktivität: 'Schwimmen' },
+      // Fügen Sie weitere Plan-Daten hier hinzu
+    ];
 
+    setUserPlan(planData);
+  }, []);
 
-ChartJS.register(
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Title,
-  Tooltip,
-  Legend
-);
-
-const LineChart = () => {
-  const data = {
-    labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'],
-    datasets: [
-      {
-        label: 'Verkäufe 2023 (Mio.)',
-        data: [12, 19, 3, 5, 2, 3],
-        backgroundColor: 'rgba(255, 255, 255, 0.5)', // Weiß mit leichter Transparenz
-        borderColor: 'rgba(211, 211, 211, 1)', // Hellgrau
-        borderWidth: 2,
-        pointBackgroundColor: 'rgba(255, 255, 255, 1)', // Weiße Datenpunkte
-        pointBorderColor: 'rgba(211, 211, 211, 1)', // Hellgraue Umrandung der Datenpunkte
-      }
-    ]
+  const navigateToSubscriptionManagement = () => {
+    // Logik zur Navigation zur Abonnentenverwaltungsseite
+    // z.B. window.location.href = '/subscription-management';
   };
 
-  const options = {
-    scales: {
-      y: {
-        beginAtZero: true
-      }
-    },
-    elements: {
-      line: {
-        tension: 0.4 // Gibt der Linie eine leichte Krümmung
-      },
-      point: {
-        radius: 5 // Größe der Datenpunkte
-      }
-    }
-  };
-
- return (
-    <div style={{
-      backgroundColor: '#2E2E2E', // Dunkelgraue Hintergrundfarbe
-      borderRadius: '15px', // Abgerundete Ecken
-      padding: '20px', // Innerer Abstand
-      maxWidth: '600px', // Maximale Breite der Box
-      margin: 'auto' // Zentriert die Box
-    }}>
-      <Line data={data} options={options} />
+  return (
+    <div>
+      <h2>Aktueller Plan</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>Tag</th>
+            <th>Aktivität</th>
+          </tr>
+        </thead>
+        <tbody>
+          {userPlan.map((item, index) => (
+            <tr key={index}>
+              <td>{item.tag}</td>
+              <td>{item.aktivität}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <button onClick={navigateToSubscriptionManagement}>
+        Zur Abonnentenverwaltung
+      </button>
     </div>
   );
 };
 
-export default LineChart;
+export default UserPlanComponent;
