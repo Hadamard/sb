6486ec6d50d062524e04c0f076610ab900d9c9da
HAL 9000 <132300160+mysteryxteo@users.noreply.github.com>
2023-12-15 03:26:47 +0100

    Update index.js
    Signed-off-by: HAL 9000 <132300160+mysteryxteo@users.noreply.github.com>

--- src/dashboard/src/pages/Overview/index.js
+++ src/dashboard/src/pages/Overview/index.js
@@ -1,98 +1,30 @@
-import React, { useState, useEffect } from "react";
-import {loadStripe} from '@stripe/stripe-js';
-import {
-  EmbeddedCheckoutProvider,
-  EmbeddedCheckout
-} from '@stripe/react-stripe-js';
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  Navigate
-} from "react-router-dom";
+import React, { useState } from 'react';
+import { Line } from 'react-chartjs-2';
 
+const TimeSeriesChart = () => {
+    const [timeInterval, setTimeInterval] = useState('weekly'); // Beispiel für einen Zustand
 
+    // Daten und Optionen für das Diagramm, basierend auf timeInterval
+    const data = {
+        // Datenstruktur hier definieren, abhängig von `timeInterval`
+    };
 
-// Make sure to call `loadStripe` outside of a component’s render to avoid
-// recreating the `Stripe` object on every render.
-// This is your test public API key.
-const stripePromise = loadStripe("pk_test_51NxABWG9TKiSYMBE6R9ITIi99Jn9e6e18LLmPnLMYfCzMCeKdPGqDtPkgTgTlPjlUWyCjAxdDC6k7GcwpcgUeqWD00RBpStmwn");
+    // Funktion zum Aktualisieren des Zeitintervalls
+    const handleIntervalChange = (event) => {
+        setTimeInterval(event.target.value);
+        // Hier sollten Sie auch die Logik zum Laden/Filtern der Daten einfügen
+    };
 
-const CheckoutForm = () => {
-  const [clientSecret, setClientSecret] = useState('');
-
-  useEffect(() => {
-    // Create a Checkout Session as soon as the page loads
-    fetch("/create-checkout-session", {
-      method: "POST",
-    })
-      .then((res) => res.json())
-      .then((data) => setClientSecret(data.clientSecret));
-  }, []);
-
-  return (
-    <div id="checkout">
-      {clientSecret && (
-        <EmbeddedCheckoutProvider
-          stripe={stripePromise}
-          options={{clientSecret}}
-        >
-          <EmbeddedCheckout />
-        </EmbeddedCheckoutProvider>
-      )}
-    </div>
-  )
-}
-
-const Return = () => {
-  const [status, setStatus] = useState(null);
-  const [customerEmail, setCustomerEmail] = useState('');
-
-  useEffect(() => {
-    const queryString = window.location.search;
-    const urlParams = new URLSearchParams(queryString);
-    const sessionId = urlParams.get('session_id');
-
-    fetch(`/session-status?session_id=${sessionId}`)
-      .then((res) => res.json())
-      .then((data) => {
-        setStatus(data.status);
-        setCustomerEmail(data.customer_email);
-      });
-  }, []);
-
-  if (status === 'open') {
     return (
-      <Navigate to="/checkout" />
-    )
-  }
+        <div>
+            <select onChange={handleIntervalChange}>
+                <option value="weekly">Wöchentlich</option>
+                <option value="monthly">Monatlich</option>
+                <option value="yearly">Jährlich</option>
+            </select>
+            <Line data={data} />
+        </div>
+    );
+};
 
-  if (status === 'complete') {
-    return (
-      <section id="success">
-        <p>
-          We appreciate your business! A confirmation email will be sent to {customerEmail}.
-
-          If you have any questions, please email <a href="mailto:orders@example.com">orders@example.com</a>.
-        </p>
-      </section>
-    )
-  }
-
-  return null;
-}
-
-const App = () => {
-  return (
-    <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/checkout" element={<CheckoutForm />} />
-          <Route path="/return" element={<Return />} />
-        </Routes>
-      </Router>
-    </div>
-  )
-}
-
-export default App;
+export default TimeSeriesChart;
