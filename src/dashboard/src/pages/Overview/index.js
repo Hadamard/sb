5e187f8bcd748a3e4d8ab209f9caa47820cd4534
HAL 9000 <132300160+mysteryxteo@users.noreply.github.com>
2023-12-16 02:21:28 +0100

    Update index.js
    Signed-off-by: HAL 9000 <132300160+mysteryxteo@users.noreply.github.com>

--- src/dashboard/src/pages/Overview/index.js
+++ src/dashboard/src/pages/Overview/index.js
@@ -1,1 +1,21 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+function App() {
+  const [number, setNumber] = useState(null);
+
+  const fetchRandomNumber = async () => {
+    const response = await axios.get('http://localhost:5000/random_number');
+    setNumber(response.data.number);
+  };
+
+  return (
+    <div>
+      <button onClick={fetchRandomNumber}>Generiere Zufallszahl</button>
+      {number !== null && <p>Zufallszahl: {number}</p>}
+    </div>
+  );
+}
+
+export default App;
 
