0fe7e9e079574b62f1eab7abbacaf13e1e4dba7d
HAL 9000 <132300160+mysteryxteo@users.noreply.github.com>
2023-12-16 20:49:58 +0100

    Update index.js
    Signed-off-by: HAL 9000 <132300160+mysteryxteo@users.noreply.github.com>

--- src/dashboard/src/pages/Overview/index.js
+++ src/dashboard/src/pages/Overview/index.js
@@ -1,20 +1,27 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
-
+const App = () => {
   const [number, setNumber] = useState(null);
 
   const fetchRandomNumber = async () => {
-    const response = await axios.get('http://localhost:5000/random_number');
-    setNumber(response.data.number);
- 
+    try {
+      const response = await axios.get('http://localhost:5000/random_number');
+      setNumber(response.data.number);
+    } catch (error) {
+      console.error('Fehler beim Abrufen der Zahl', error);
+      // Optional: Fehlerbehandlung hinzufügen
+    }
+  };
 
   return (
-  <div>
-        {number !== null ? <p>Abgerufene Nummer: {number}</p> : <p>Noch keine Zahl abgerufen.</p>}
-      </div>
+    <div>
+      {number !== null ? <p>Abgerufene Nummer: {number}</p> : <p>Noch keine Zahl abgerufen.</p>}
+      <button onClick={fetchRandomNumber}>Zahl abrufen</button>
+    </div>
   );
-}
+};
 
 export default App;
+
 
