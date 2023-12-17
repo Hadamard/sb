883c0668ed403cd8cabdde3551dd70e37bb96ac9
HAL 9000 <132300160+mysteryxteo@users.noreply.github.com>
2023-12-17 17:42:21 +0100

    Update SiderMenu.js
    Signed-off-by: HAL 9000 <132300160+mysteryxteo@users.noreply.github.com>

--- src/dashboard/src/components/SiderMenu/SiderMenu.js
+++ src/dashboard/src/components/SiderMenu/SiderMenu.js
@@ -96,15 +96,7 @@ export default class SiderMenu extends PureComponent {
           />
         </Suspense>
 
-    <div className="columns">
-      <ul className="price">
-        <li className="header">Free</li>
-        <li className="grey">$ 0.00 / month</li>
-        <li className="grey">
-          <a href="https://billing.stripe.com/p/login/aEU2bs5DY0Sva2ccMM" className="button">Manage plan</a>
-        </li>
-      </ul>
-    </div>
+   
  
       </Sider>
     );
