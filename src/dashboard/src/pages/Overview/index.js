8f8ce0d71b7a5b97f1b49c9bba02acf927ede309
HAL 9000 <132300160+mysteryxteo@users.noreply.github.com>
2023-12-17 19:27:07 +0100

    Update index.js
    Signed-off-by: HAL 9000 <132300160+mysteryxteo@users.noreply.github.com>

--- src/dashboard/src/pages/Overview/index.js
+++ src/dashboard/src/pages/Overview/index.js
@@ -1,28 +1,81 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
-const BitcoinPrice = () => {
-  const [price, setPrice] = useState(null);
+import './styles.css';
 
-  useEffect(() => {
-    fetch('https://api.coindesk.com/v1/bpi/currentprice.json')
-      .then(response => response.json())
-      .then(data => {
-        setPrice(data.bpi.USD.rate);
-      })
-      .catch(error => console.error('Error fetching data: ', error));
-  }, []);
 
+
+
+const ContactForm = () => {
+  
+
+  
   return (
-    <div>
-      <h1>Current Bitcoin Price</h1>
-      <p>{price ? `$${price}` : 'Loading...'}</p>
+<div>
+
+
+
+    
+     <div className="columns">
+      <ul className="price">
+        <li className="header">Free</li>
+        <li className="grey">$ 0.00 / month</li>
+        <li className="grey">
+          <a href="https://billing.stripe.com/p/login/aEU2bs5DY0Sva2ccMM" className="button">Manage plan</a>
+        </li>
+      </ul>
     </div>
-  );
-}
+  
+  
 
-export default BitcoinPrice;
+ <div>
+
+
+    <div className="login-container">
+        <h1>Support Ticket</h1>
+        <div className="form">
+            <label for="username">Name:</label>
+            <input type="text" id="username" name="username" required/>
+            
+            <label for="password">E-Mail:</label>
+            <input type="email" id="password" name="password" required/>
+            
+            <label for="subject">Subject:</label>
+            <input type="subject" id="sub" name="sub" required />
+
+            <label for="message">Message:</label>
+            <input type="message" id="message" name="message" required/>
+            
+            <div className="textbox-container">
+              <button type="submit" className="progressbar-container">
+                
+                <div className="progressbar" id="progressbar">Send</div>
+              </button>
+            </div>
+        
+        
+          </div>
+
+   
+    </div>
+
+
+</div>
 
 
 
+</div>
+
+
+
+
+  
  
+  );
+};
+
+export default ContactForm;
+
+  
+
+
 
