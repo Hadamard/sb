6b0c584495c483b61d0397e3217fe19b3f59a4ce
HAL 9000 <132300160+mysteryxteo@users.noreply.github.com>
2023-12-18 05:05:23 +0100

    Update UserLayout.js
    Signed-off-by: HAL 9000 <132300160+mysteryxteo@users.noreply.github.com>

--- src/dashboard/src/layouts/UserLayout.js
+++ src/dashboard/src/layouts/UserLayout.js
@@ -10,49 +10,96 @@ const links = [];
 
 const copyright = <Fragment>Hadamard Corporation © 2024</Fragment>;
 
-class UserLayout extends Component {
-  componentDidMount() {
-    const {
-      dispatch,
-      route: { routes, authority },
-    } = this.props;
-    dispatch({
-      type: 'menu/getMenuData',
-      payload: { routes, authority },
-    });
-  }
 
-  render() {
-    const {
-      children,
-      location: { pathname },
-      breadcrumbNameMap,
-    } = this.props;
-    return (
-      <DocumentTitle title={getPageTitle(pathname, breadcrumbNameMap)}>
-        <div className={styles.container}>
-          <div className={styles.lang}>
-            <SelectLang />
-          </div>
-          <div className={styles.content}>
-            <div className={styles.top}>
-              <div className={styles.header}>
-                <Link to="/">
-                  <span className={styles.title}>Hadamard Dashboard</span>
-                </Link>
+
+
+
+const texts = [">> Hadamard Hyperledger leverages Quantum Key Distribution (QKD) technology to enhance the security of blockchain transactions.",
+ ">> By incorporating the Hyperledger project, Hadamard Hyperledger provides a robust and scalable blockchain framework.",
+  ">>  The combination of QKD and blockchain in Hadamard Hyperledger ensures superior data privacy and integrity.",
+   ">>  With its quantum-resistant cryptographic methods, Hadamard Hyperledger positions itself as a future-proof solution.",
+    ">>  The framework can be utilized for various applications."];
+
+
+
+
+
+
+
+    const Typewriter = () => {
+      const [index, setIndex] = useState(0);
+      const [text, setText] = useState("");
+      const intervalRef = React.useRef(null);
+    
+      const magicName = useTypewriter(text);
+    
+      useEffect(() => {
+        if (text === texts[index]) {
+          clearInterval(intervalRef.current);
+          intervalRef.current = setTimeout(() => {
+            setIndex((prevIndex) => (prevIndex + 1) % texts.length);
+          }, 30000); // Ändert den Text nach 5 Sekunden
+        } else {
+          setText(texts[index]);
+        }
+      }, [text, index]);
+    
+      useEffect(() => {
+        return () => {
+          clearInterval(intervalRef.current);
+        };
+      }, []);
+    
+      return <div className='wrap'>{magicName}</div>;
+    };
+    
+
+     
+    
+    
+    class UserLayout extends Component {
+      componentDidMount() {
+        const {
+          dispatch,
+          route: { routes, authority },
+        } = this.props;
+        dispatch({
+          type: 'menu/getMenuData',
+          payload: { routes, authority },
+        });
+      }
+
+      render() {
+        const {
+          children,
+          location: { pathname },
+          breadcrumbNameMap,
+        } = this.props;
+        return (
+          <DocumentTitle title={getPageTitle(pathname, breadcrumbNameMap)}>
+            <div className={styles.container}>
+              <div className={styles.lang}>
+                <SelectLang />
               </div>
-              <div className={styles.desc}>Dashboard for Managing Hadamard Hyperledger</div>
+              <div className={styles.content}>
+                <div className={styles.top}>
+                  <div className={styles.header}>
+                    <Link to="/">
+                      <span className={styles.title}>Hadamard Dashboard</span>
+                    </Link>
+                  </div>
+                  <div className={styles.desc}>Dashboard for Managing Hadamard Hyperledger</div>
+                </div>
+                {children}
+              </div>
+              <GlobalFooter links={links} copyright={copyright} />
             </div>
-            {children}
-          </div>
-          <GlobalFooter links={links} copyright={copyright} />
-        </div>
-      </DocumentTitle>
-    );
-  }
-}
-
-export default connect(({ menu: menuModel }) => ({
-  menuData: menuModel.menuData,
-  breadcrumbNameMap: menuModel.breadcrumbNameMap,
-}))(UserLayout);
+          </DocumentTitle>
+        );
+      }
+    }
+    
+    export default connect(({ menu: menuModel }) => ({
+      menuData: menuModel.menuData,
+      breadcrumbNameMap: menuModel.breadcrumbNameMap,
+    }))(UserLayout);
