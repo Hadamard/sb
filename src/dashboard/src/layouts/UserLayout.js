447e659beedf1476ad7f100de17ec2d9b251d03c
HAL 9000 <132300160+mysteryxteo@users.noreply.github.com>
2023-12-18 04:57:29 +0100

    Update UserLayout.js
    Signed-off-by: HAL 9000 <132300160+mysteryxteo@users.noreply.github.com>

--- src/dashboard/src/layouts/UserLayout.js
+++ src/dashboard/src/layouts/UserLayout.js
@@ -1,27 +1,14 @@
-import React, { Component, Fragment, useState, useEffect } from 'react';
+import React, { Component, Fragment } from 'react';
 import { Link, connect } from 'umi';
 import GlobalFooter from '@/components/GlobalFooter';
 import DocumentTitle from 'react-document-title';
 import SelectLang from '@/components/SelectLang';
 import getPageTitle from '@/utils/getPageTitle';
 import styles from './UserLayout.less';
-import useTypewriter from 'react-typewriter-hook';
-
 
 const links = [];
 
 const copyright = <Fragment>Hadamard Corporation © 2024</Fragment>;
-
-
-const texts = [">> Hadamard Hyperledger leverages Quantum Key Distribution (QKD) technology to enhance the security of blockchain transactions.",
-">> By incorporating the Hyperledger project, Hadamard Hyperledger provides a robust and scalable blockchain framework.",
-">>  The combination of QKD and blockchain in Hadamard Hyperledger ensures superior data privacy and integrity.",
-">>  With its quantum-resistant cryptographic methods, Hadamard Hyperledger positions itself as a future-proof solution.",
-">>  The framework can be utilized for various applications."];
-
-
-
-
 
 class UserLayout extends Component {
   componentDidMount() {
@@ -33,50 +20,8 @@ class UserLayout extends Component {
       type: 'menu/getMenuData',
       payload: { routes, authority },
     });
-  };
+  }
 
-
-
-
-
-const Typewriter = () => {
-  const [index, setIndex] = useState(0);
-  const [text, setText] = useState("");
-  const intervalRef = React.useRef(null);
-
-  const magicName = useTypewriter(text);
-
- 
-  
-  
-  
-  useEffect(() => {
-    if (text === texts[index]) {
-      clearInterval(intervalRef.current);
-      intervalRef.current = setTimeout(() => {
-        setIndex((prevIndex) => (prevIndex + 1) % texts.length);
-      }, 30000); // Ändert den Text nach 5 Sekunden
-    } else {
-      setText(texts[index]);
-    }
-  }, [text, index]);
-
-  useEffect(() => {
-    return () => {
-      clearInterval(intervalRef.current);
-    };
-  }, []);
-return  <div className='wrap'>{magicName}</div>;
-};
-
-
-
-
-
-
-
-  
-  
   render() {
     const {
       children,
@@ -100,7 +45,6 @@ return  <div className='wrap'>{magicName}</div>;
             </div>
             {children}
           </div>
-            <div className='wrap'>{magicName}</div>
           <GlobalFooter links={links} copyright={copyright} />
         </div>
       </DocumentTitle>
