2f56eee167570a0faea5537f53a7b45ba9a0dbf4
HAL 9000 <132300160+mysteryxteo@users.noreply.github.com>
2023-12-18 16:51:24 +0100

    Update index.js
    Signed-off-by: HAL 9000 <132300160+mysteryxteo@users.noreply.github.com>

--- src/dashboard/src/pages/Overview/index.js
+++ src/dashboard/src/pages/Overview/index.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 
 import './styles.css';
-
+import MyChartComponent from '@/Overview/MyChartComponent';
 
 
 
@@ -62,6 +62,19 @@ const ContactForm = () => {
 </div>
 
 
+
+
+
+
+<MyChartComponent> </MyChartComponent>
+
+
+
+
+
+
+
+  
 
 </div>
 
