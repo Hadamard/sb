3c9a7f94a840d27841352f69a865419680e6a610
HAL 9000 <132300160+mysteryxteo@users.noreply.github.com>
2023-12-18 03:58:02 +0100

    Update UserLayout.js
    Signed-off-by: HAL 9000 <132300160+mysteryxteo@users.noreply.github.com>

--- src/dashboard/src/layouts/UserLayout.js
+++ src/dashboard/src/layouts/UserLayout.js
@@ -5,6 +5,8 @@ import DocumentTitle from 'react-document-title';
 import SelectLang from '@/components/SelectLang';
 import getPageTitle from '@/utils/getPageTitle';
 import styles from './UserLayout.less';
+import useTypewriter from 'react-typewriter-hook';
+
 
 const links = [];
 
@@ -23,6 +25,53 @@ class UserLayout extends Component {
   }
 
 
+
+
+
+const texts = [">> Hadamard Hyperledger leverages Quantum Key Distribution (QKD) technology to enhance the security of blockchain transactions.",
+ ">> By incorporating the Hyperledger project, Hadamard Hyperledger provides a robust and scalable blockchain framework.",
+  ">>  The combination of QKD and blockchain in Hadamard Hyperledger ensures superior data privacy and integrity.",
+   ">>  With its quantum-resistant cryptographic methods, Hadamard Hyperledger positions itself as a future-proof solution.",
+    ">>  The framework can be utilized for various applications."];
+
+const Typewriter = () => {
+  const [index, setIndex] = useState(0);
+  const [text, setText] = useState("");
+  const intervalRef = React.useRef(null);
+
+  const magicName = useTypewriter(text);
+
+ 
+  
+  
+  
+  useEffect(() => {
+    if (text === texts[index]) {
+      clearInterval(intervalRef.current);
+      intervalRef.current = setTimeout(() => {
+        setIndex((prevIndex) => (prevIndex + 1) % texts.length);
+      }, 30000); // Ändert den Text nach 5 Sekunden
+    } else {
+      setText(texts[index]);
+    }
+  }, [text, index]);
+
+  useEffect(() => {
+    return () => {
+      clearInterval(intervalRef.current);
+    };
+  }, []);
+
+};
+
+
+
+
+
+
+
+
+  
   
   render() {
     const {
@@ -47,7 +96,7 @@ class UserLayout extends Component {
             </div>
             {children}
           </div>
-            
+            <div className='wrap'>{magicName}</div>
           <GlobalFooter links={links} copyright={copyright} />
         </div>
       </DocumentTitle>
