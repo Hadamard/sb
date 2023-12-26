17741564b680ec7f912ae6513c08984cb2c502a3
HAL 9000 <132300160+mysteryxteo@users.noreply.github.com>
2023-12-26 02:34:55 +0100

    Update index.js
    Signed-off-by: HAL 9000 <132300160+mysteryxteo@users.noreply.github.com>

--- src/dashboard/src/components/Hubspot/index.js
+++ src/dashboard/src/components/Hubspot/index.js
@@ -1,1 +1,29 @@
 
+import React, { useEffect } from 'react';
+
+const Hubspot = () => {
+  useEffect(() => {
+    // Füge den HubSpot-Code hier ein
+    const script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.id = 'hs-script-loader';
+    script.async = true;
+    script.defer = true;
+    script.src = '//js-eu1.hs-scripts.com/143301083.js';
+    document.head.appendChild(script);
+
+    // Optional: Entferne das Skript beim Komponentenabbau, um mögliche Probleme zu vermeiden
+    return () => {
+      document.head.removeChild(script);
+    };
+  }, []); // Das leere Array stellt sicher, dass der Effekt nur einmalig beim Montieren der Komponente ausgeführt wird
+
+  return (
+    <div>
+      {/* Hier kannst du den restlichen React-Code für deine Komponente hinzufügen */}
+      <p>Deine React-Komponente mit HubSpot-Integration</p>
+    </div>
+  );
+};
+
+export default Hubspot;
