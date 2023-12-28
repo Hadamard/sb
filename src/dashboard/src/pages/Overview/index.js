d7ad021e55a3b17030d26d6b660a62702cbd4b80
HAL 9000 <132300160+mysteryxteo@users.noreply.github.com>
2023-12-28 05:02:12 +0100

    Update index.js
    Signed-off-by: HAL 9000 <132300160+mysteryxteo@users.noreply.github.com>

--- src/dashboard/src/pages/Overview/index.js
+++ src/dashboard/src/pages/Overview/index.js
@@ -49,15 +49,9 @@ function SupportTicketForm() {
 
 
     
-     <div className="columns">
-      <ul className="price">
-        <li className="header">Free</li>
-        <li className="grey">$ 0.00 / month</li>
-        <li className="grey">
-          <a href="https://billing.stripe.com/p/login/aEU2bs5DY0Sva2ccMM" className="button">Manage plan</a>
-        </li>
-      </ul>
-    </div>
+    <Elements stripe={stripePromise}>
+      <stripe />
+    </Elements>
   
   
 
