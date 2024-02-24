24d64d5153fdfa4debcd71fbb046bd7cfccdd7a5
HAL 9000 <132300160+mysteryxteo@users.noreply.github.com>
2024-02-24 20:44:01 +0100

    Update CostManagement.js
    Signed-off-by: HAL 9000 <132300160+mysteryxteo@users.noreply.github.com>

--- src/dashboard/src/pages/CostManagement/CostManagement.js
+++ src/dashboard/src/pages/CostManagement/CostManagement.js
@@ -2,10 +2,20 @@ import React from 'react';
 
 class cost extends React.Component {
   render() {
+    const iframeStyle = {
+      width: '100%',
+      height: '100%',
+      border: 'none' // optional: removes iframe border
+    };
+
     return (
-      <div>
-       
+      <div style={{ width: '100%', height: '100%' }}>
     
+        <iframe
+          title="Documentation"
+          src="http://bb84.com/dash"
+          style={iframeStyle}
+        />
       </div>
     );
   }
