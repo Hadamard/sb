25b05574e0313337c7af50e4d780e8364b46d2a3
HAL 9000 <132300160+mysteryxteo@users.noreply.github.com>
2024-02-25 17:21:45 +0100

    Create Organization.js
    Signed-off-by: HAL 9000 <132300160+mysteryxteo@users.noreply.github.com>

---
+++ src/dashboard/src/locales/fr-FR/Organization.js
@@ -0,0 +1,17 @@
+export default {
+  'app.organization.title': 'Gestion des Organisations',
+  'app.organization.table.header.name': 'Nom de l\'Organisation',
+  'app.organization.table.header.createTime': 'Heure de Création',
+  'app.organization.form.new.title': 'Nouvelle Organisation',
+  'app.organization.form.update.title': 'Mettre à Jour l\'Organisation',
+  'app.organization.form.name.label': 'Nom de l\'Organisation',
+  'app.organization.form.name.required': 'Veuillez saisir le nom de l\'organisation',
+  'app.organization.create.success': 'Création de l\'organisation {name} réussie',
+  'app.organization.create.fail': 'Échec de la création de l\'organisation {name}',
+  'app.organization.update.success': 'Mise à jour de l\'organisation {name} réussie',
+  'app.organization.update.fail': 'Échec de la mise à jour de l\'organisation {name}',
+  'app.organization.form.delete.title': 'Supprimer l\'Organisation',
+  'app.organization.form.delete.content': 'Confirmez la suppression de l\'organisation {name}',
+  'app.organization.delete.fail': 'Échec de la suppression de l\'organisation {name}',
+  'app.organization.delete.success': 'Suppression de l\'organisation {name} réussie',
+};
