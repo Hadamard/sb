09e43e95658461114ed87ba1e162229b85ceb450
HAL 9000 <132300160+mysteryxteo@users.noreply.github.com>
2024-02-25 16:31:48 +0100

    Update component.js
    Signed-off-by: HAL 9000 <132300160+mysteryxteo@users.noreply.github.com>

--- src/dashboard/src/locales/de-DE/component.js
+++ src/dashboard/src/locales/de-DE/component.js
@@ -1,1 +1,12 @@
+export default {
+  'component.tagSelect.expand': 'Erweitern',
+  'component.tagSelect.collapse': 'Einklappen',
+  'component.tagSelect.all': 'Alle',
+  'component.miniProgress.tooltipDefault': 'Zielwert',
+  'component.standardTable.selected': 'Ausgewählt',
+  'component.standardTable.item': 'Element',
+  'component.standardTable.total': 'Gesamt',
+  'component.standardTable.clean': 'Löschen',
+  'component.standardTable.showTotal': '{start}-{end} von {total} Elementen',
+};
 
