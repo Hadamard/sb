f4356eb3d5ac3ea76e58822f6080fccbc72c2506
HAL 9000 <132300160+mysteryxteo@users.noreply.github.com>
2024-02-25 16:33:05 +0100

    Update form.js
    Signed-off-by: HAL 9000 <132300160+mysteryxteo@users.noreply.github.com>

--- src/dashboard/src/locales/de-DE/form.js
+++ src/dashboard/src/locales/de-DE/form.js
@@ -1,1 +1,19 @@
+export default {
+  'form.button.new': 'Neu',
+  'form.table.header.operation': 'Operation',
+  'form.menu.item.download': 'Herunterladen',
+  'form.menu.item.upload': 'Hochladen',
+  'form.menu.item.joinChannel': 'Channel beitreten',
+  'form.menu.item.delete': 'Löschen',
+  'form.input.placeholder': 'Bitte eingeben',
+  'form.menu.item.update': 'Aktualisieren',
+  'form.button.confirm': 'Bestätigen',
+  'form.button.cancel': 'Abbrechen',
+  'form.button.moreActions': 'Weitere Aktionen',
+  'form.button.submit': 'Absenden',
+  'form.button.add': 'Hinzufügen',
+  'form.button.back': 'Zurück',
+  'form.button.prev': 'Zurück',
+  'form.button.next': 'Weiter',
+};
 
