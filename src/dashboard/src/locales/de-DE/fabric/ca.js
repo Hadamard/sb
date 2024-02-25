07c680f2fe138f352664bfc049ad00c76a21a19a
HAL 9000 <132300160+mysteryxteo@users.noreply.github.com>
2024-02-25 16:43:10 +0100

    Create ca.js
    Signed-off-by: HAL 9000 <132300160+mysteryxteo@users.noreply.github.com>

---
+++ src/dashboard/src/locales/de-DE/fabric/ca.js
@@ -0,0 +1,11 @@
+export default {
+  'fabric.ca.form.adminName.label': 'Administrator-Benutzername',
+  'fabric.ca.form.adminName.placeholder': 'Administrator-Benutzernamen eingeben',
+  'fabric.ca.form.adminName.required': 'Bitte geben Sie den Administrator-Benutzernamen ein',
+  'fabric.ca.form.adminPassword.label': 'Administrator-Passwort',
+  'fabric.ca.form.adminPassword.placeholder': 'Administrator-Passwort eingeben',
+  'fabric.ca.form.adminPassword.required': 'Bitte geben Sie das Administrator-Passwort ein',
+  'fabric.ca.form.caType.label': 'CA-Typ',
+  'fabric.ca.form.caType.placeholder': 'CA-Typ auswählen',
+  'fabric.ca.form.caType.required': 'Bitte wählen Sie den CA-Typ aus',
+};
