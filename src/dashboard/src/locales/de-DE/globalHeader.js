bf71ef0ccf018d5d92acc80cde5310438e41d295
HAL 9000 <132300160+mysteryxteo@users.noreply.github.com>
2024-02-25 16:33:38 +0100

    Update globalHeader.js
    Signed-off-by: HAL 9000 <132300160+mysteryxteo@users.noreply.github.com>

--- src/dashboard/src/locales/de-DE/globalHeader.js
+++ src/dashboard/src/locales/de-DE/globalHeader.js
@@ -1,1 +1,18 @@
+export default {
+  'component.globalHeader.search': 'Suche',
+  'component.globalHeader.search.example1': 'Suchbeispiel 1',
+  'component.globalHeader.search.example2': 'Suchbeispiel 2',
+  'component.globalHeader.search.example3': 'Suchbeispiel 3',
+  'component.globalHeader.help': 'Hilfe',
+  'component.globalHeader.notification': 'Benachrichtigung',
+  'component.globalHeader.notification.empty': 'Du hast alle Benachrichtigungen angesehen.',
+  'component.globalHeader.message': 'Nachricht',
+  'component.globalHeader.message.empty': 'Du hast alle Nachrichten angesehen.',
+  'component.globalHeader.event': 'Ereignis',
+  'component.globalHeader.event.empty': 'Du hast alle Ereignisse angesehen.',
+  'component.noticeIcon.clear': 'Löschen',
+  'component.noticeIcon.cleared': 'Gelöscht',
+  'component.noticeIcon.empty': 'Keine Benachrichtigungen',
+  'component.noticeIcon.view-more': 'Mehr anzeigen',
+};
 
