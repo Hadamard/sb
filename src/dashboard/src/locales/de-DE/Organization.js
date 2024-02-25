bfbfd54f601719f7c4e1c5b26d10e579157fee17
HAL 9000 <132300160+mysteryxteo@users.noreply.github.com>
2024-02-25 16:31:10 +0100

    Update Organization.js
    Signed-off-by: HAL 9000 <132300160+mysteryxteo@users.noreply.github.com>

--- src/dashboard/src/locales/de-DE/Organization.js
+++ src/dashboard/src/locales/de-DE/Organization.js
@@ -1,1 +1,18 @@
+export default {
+  'app.organization.title': 'Organisationsverwaltung',
+  'app.organization.table.header.name': 'Organisationsname',
+  'app.organization.table.header.createTime': 'Erstellungszeit',
+  'app.organization.form.new.title': 'Neue Organisation',
+  'app.organization.form.update.title': 'Organisation aktualisieren',
+  'app.organization.form.name.label': 'Organisationsname',
+  'app.organization.form.name.required': 'Bitte geben Sie den Organisationsnamen ein',
+  'app.organization.create.success': 'Organisation {name} erfolgreich erstellt',
+  'app.organization.create.fail': 'Erstellen der Organisation {name} fehlgeschlagen',
+  'app.organization.update.success': 'Organisation {name} erfolgreich aktualisiert',
+  'app.organization.update.fail': 'Aktualisieren der Organisation {name} fehlgeschlagen',
+  'app.organization.form.delete.title': 'Organisation löschen',
+  'app.organization.form.delete.content': 'Möchten Sie die Organisation {name} wirklich löschen?',
+  'app.organization.delete.fail': 'Löschen der Organisation {name} fehlgeschlagen',
+  'app.organization.delete.success': 'Organisation {name} erfolgreich gelöscht',
+};
 
