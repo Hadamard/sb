27022c1b884a10decd358d28db7f21a737d0ec34
HAL 9000 <132300160+mysteryxteo@users.noreply.github.com>
2024-02-25 17:22:54 +0100

    Create exception.js
    Signed-off-by: HAL 9000 <132300160+mysteryxteo@users.noreply.github.com>

---
+++ src/dashboard/src/locales/fr-FR/exception.js
@@ -0,0 +1,6 @@
+export default {
+  'app.exception.back': 'Retour à l\'accueil',
+  'app.exception.description.403': 'Désolé, vous n\'avez pas accès à cette page',
+  'app.exception.description.404': 'Désolé, la page que vous avez visitée n\'existe pas',
+  'app.exception.description.500': 'Désolé, le serveur signale une erreur',
+};
