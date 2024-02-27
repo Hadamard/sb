b2601d0a1e0908914d78f29ff4cd7d7989931bad
HAL 9000 <132300160+mysteryxteo@users.noreply.github.com>
2024-02-27 08:40:09 +0100

    Update CostManagement.js
    Signed-off-by: HAL 9000 <132300160+mysteryxteo@users.noreply.github.com>

--- src/dashboard/src/pages/CostManagement/CostManagement.js
+++ src/dashboard/src/pages/CostManagement/CostManagement.js
@@ -54,19 +54,7 @@ const CreateUpdateForm = props => {
   );
 };
 
-@connect(({ user, organization, loading }) => ({
-  user,
-  organization,
-  loadingUsers: loading.effects['user/fetch'],
-  creatingUser: loading.effects['user/createUser'],
-}))
-class UserManagement extends PureComponent {
-  state = {
-    modalVisible: false,
-    modalMethod: 'create',
-    selectedRows: [],
-    // formValues: {},
-  };
+
 
   componentDidMount() {
     const { dispatch } = this.props;
