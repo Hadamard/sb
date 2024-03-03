884aedffb816c6a21403d2d2014d2cbf0416361e
HAL 9000 <132300160+mysteryxteo@users.noreply.github.com>
2024-03-03 17:01:27 +0100

    Update CostManagement.js
    Signed-off-by: HAL 9000 <132300160+mysteryxteo@users.noreply.github.com>

--- src/dashboard/src/pages/CostManagement/CostManagement.js
+++ src/dashboard/src/pages/CostManagement/CostManagement.js
@@ -1,20 +1,12 @@
 import React, { PureComponent, Fragment } from 'react';
 import { connect, injectIntl } from 'umi';
-import { Card, Button, Form, Modal, Input, message, Divider } from 'antd';
-import { PlusOutlined, TeamOutlined } from '@ant-design/icons';
-import moment from 'moment';
-import PageHeaderWrapper from '@/components/PageHeaderWrapper';
-import RedTable from '@/components/RedTable';
-import styles from '../styles.less';
-
 @connect(({channel, network, node, user, loading }) => ({
   channel,
   node,
   user,
   network,
 }))
-
-  
+         
 class UserManagement extends PureComponent {
     
 componentDidMount() {
@@ -40,26 +32,8 @@ componentDidMount() {
 
     return (
       <div>
-     
-     
-        <Card bordered={false}>
-          <div className={styles.tableList}>
-        
-            <RedTable
-         
-              data={{
-      
-              }}
-          
-            />
-          </div>
-        </Card>
-      
-      
-      
-      
-      <a href="https://hadamard.com">
-      <button>Go to Destination</button>
+     <a href="https://hadamard.com">
+      <button>Change Plan</button>
     </a>
     </div>
     );
