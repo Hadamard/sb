6d282af98c250791ce1fa81afe5491d204ea8d73
HAL 9000 <132300160+mysteryxteo@users.noreply.github.com>
2024-03-03 16:35:37 +0100

    Create test.js
    Signed-off-by: HAL 9000 <132300160+mysteryxteo@users.noreply.github.com>

---
+++ src/dashboard/src/pages/CostManagement/test.js
@@ -0,0 +1,42 @@
+import React, { PureComponent, Fragment } from 'react';
+import { connect, injectIntl } from 'umi';
+@connect(({channel, network, node, user, loading }) => ({
+  channel,
+  node,
+  user,
+  network,
+}))
+         
+class UserManagement extends PureComponent {
+    
+componentDidMount() {
+  const { dispatch } = this.props;
+  dispatch({ type: 'user/fetch' });
+  dispatch({ type: 'network/listNetwork' });
+  dispatch({ type: 'node/listNode' });
+  dispatch({ type: 'channel/listChannel' });
+}
+  render() {
+    
+    const {
+      user: { users, pagination, currentUser },
+      node: { nodes },
+      network: { networks },
+      channel: { channels }, 
+    } = this.props;
+   
+
+
+    console.log(this.props);
+   
+
+    return (
+      <div>
+     <a href="https://hadamard.com">
+      <button>Go to Destination</button>
+    </a>
+    </div>
+    );
+  }
+}
+export default injectIntl(UserManagement);
