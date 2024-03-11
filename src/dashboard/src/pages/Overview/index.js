5054455667f522d74dfd2e09990dbd3d89b87a17
HAL 9000 <132300160+mysteryxteo@users.noreply.github.com>
2024-03-11 19:43:55 +0100

    Update index.js
    Signed-off-by: HAL 9000 <132300160+mysteryxteo@users.noreply.github.com>

--- src/dashboard/src/pages/Overview/index.js
+++ src/dashboard/src/pages/Overview/index.js
@@ -111,7 +111,106 @@ componentDidMount() {
 
 
     return (
-@@ -235,7 +235,7 @@ componentDidMount() {
+ 
+    <div className='App'>
+     <head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" />
+<link href="https://unicons.iconscout.com/release/v3.0.6/css/line.css" rel="stylesheet" />
+</head>
+  
+  <section id="wrapper">  
+  
+    <div className="p-4">
+      <div className="welcome">
+        <div className="content rounded-3 p-3">
+          <h1 className="fs-3">Hadamard Hyperledger</h1>
+          <p className="mb-0">Welcome to the Hyperledger dashboard, {currentUser.username}!</p>
+        </div>
+      </div>
+  
+      <section className="statistics mt-4">
+        <div className="row">
+          <div className="col-lg-4">
+            <div className="box d-flex rounded-2 align-items-center mb-4 mb-lg-0 p-3">
+             
+              <div className="ms-3">
+                <div className="d-flex align-items-center">
+                  <h3 className="mb-0">{nodes.length}</h3> <span className="d-block ms-2">Nodes</span>
+                </div>
+                <p className="fs-normal mb-0">Nodes connected to your account</p>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-4">
+            <div className="box d-flex rounded-2 align-items-center mb-4 mb-lg-0 p-3">
+            
+              <div className="ms-3">
+                <div className="d-flex align-items-center">
+                  <h3 className="mb-0">{networks.length}</h3> <span className="d-block ms-2">Networks</span>
+                </div>
+                <p className="fs-normal mb-0">Networks connected to your account</p>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-4">
+            <div className="box d-flex rounded-2 align-items-center p-3">
+             
+              <div className="ms-3">
+                <div className="d-flex align-items-center">
+                  <h3 className="mb-0">{channels.length}</h3> <span className="d-block ms-2">Channels</span>
+                </div>
+                <p className="fs-normal mb-0">Channels connected to your account</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+  
+      <section className="charts mt-4">
+        <div className="row">
+          <div className="col-lg-6">
+            <div className="chart-container rounded-2 p-3">
+              <h3 className="fs-6 mb-3">Network requests</h3>
+           <BarChart />
+            </div>
+          </div>
+          <div className="col-lg-6">
+            <div className="chart-container rounded-2 p-3">
+              <h3 className="fs-6 mb-3">Transactions</h3>
+                <LineChart />
+            </div>
+          </div>
+        </div>
+      </section>
+  
+      <section className="admins mt-4">
+        <div className="row">
+          <div className="col-md-6">
+            <div className="box">
+            
+              
+            </div>
+          </div>
+         
+        </div>
+      </section>
+  
+      <section className="statis mt-4 text-center">
+        <div className="row">
+          <div className="col-md-6 col-lg-3 mb-4 mb-lg-0">
+            <div className="box bg-primary p-3">
+            
+              <h3>Healthy</h3>
+              <p className="lead">Status</p>
+            </div>
+          </div>
+          <div className="col-md-6 col-lg-3 mb-4 mb-lg-0">
+            <div className="box bg-danger p-3">
+             
+              <h3>245</h3>
+              <p className="lead">Network Requests</p>
+            </div>
+          </div>
           <div className="col-md-6 col-lg-3 mb-4 mb-md-0">
             <div className="box bg-warning p-3">
 
